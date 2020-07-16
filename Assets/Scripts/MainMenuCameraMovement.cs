64353b4538d45eb8048ed27358367e0835f90cab
Dawid Stefański <ddstefanski@gmail.com>
2020-07-16 02:33:41 +0200

    Setting up git repository
    Setting up git repository for the first time. Uploading project files.

---
+++ Assets/Scripts/MainMenuCameraMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed = 180f;
+
+    bool moveToGarage = false;
+    bool moveToMenuFromGarage = false;
+    bool moveToShop = false;
+    bool moveToMenuFromShop = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(transform.rotation.y);
+        
+
+        if (moveToGarage == true)
+            {
+                if (transform.rotation.y < 0.89)
+                {
+                    transform.Rotate(0, Time.deltaTime * movementSpeed, 0);
+                }
+                else moveToGarage = false;
+              
+            }
+
+
+        if (moveToMenuFromGarage == true)
+        {
+            if (transform.rotation.y > 0.525)
+            {
+                transform.Rotate(0, Time.deltaTime * -movementSpeed, 0);
+            }
+            else moveToMenuFromGarage = false;
+
+        }
+
+        if (moveToShop == true)
+        {
+            if (transform.rotation.y > 0.10)
+            {
+                transform.Rotate(0, Time.deltaTime * -movementSpeed, 0);
+            }
+            else moveToShop = false;
+
+        }
+
+
+        if (moveToMenuFromShop == true)
+        {
+            if (transform.rotation.y < 0.525)
+            {
+                transform.Rotate(0, Time.deltaTime * movementSpeed, 0);
+            }
+            else moveToMenuFromShop = false;
+
+        }
+
+
+    }
+
+    public void ViewGarage()
+    {
+        moveToGarage = true;
+    }
+    public void ViewMenuFromGarage()
+    {
+        moveToMenuFromGarage = true;
+    }
+    public void ViewShop()
+    {
+        moveToShop = true;
+    }
+    public void ViewMenuFromShop()
+    {
+        moveToMenuFromShop = true;
+    }
+
+}
---
+++ Assets/Scripts/SteeringScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SteeringScript : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 5;
+    [SerializeField] float acceleration = 8;
+
+    Quaternion targetRotation;
+    Rigidbody vehicle;
+
+    Vector3 lastPosition;
+  
+
+
+    protected Joystick joystick;
+    float joyH;
+    float joyV;
+
+
+    private void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        vehicle = GetComponent<Rigidbody>();
+    }
+
+    
+
+    void GetJoystickPosition()
+    {
+        joyH = joystick.Horizontal;
+        joyV = joystick.Vertical;
+    }
+
+    void Update()
+    {
+      
+        SetRotationPoint();
+       
+        
+    }
+
+  
+
+
+
+    void FixedUpdate()
+    {
+        GetJoystickPosition();
+                     
+        // float accelerationInput = acceleration * (Input.GetMouseButton(0) ? 1 : Input.GetMouseButton(1) ? -1 : 0) * Time.fixedDeltaTime;
+        float accelerationInput = acceleration * ((joyH != 0 || joyV != 0) ? 1 : (joyH == 0 || joyV == 0) ? 0 : 0) * Time.fixedDeltaTime;
+
+        vehicle.AddRelativeForce(Vector3.forward * accelerationInput );
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.fixedDeltaTime * turnSpeed );
+
+    }
+
+    private void SetRotationPoint()
+    {
+        
+        if (joyH!=0 || joyV!=0)
+        {
+            Vector3 target = new Vector3(joyH*100f, 0, joyV*100f); //JAK TO DZIALA XDDD
+            Vector3 direction = target - transform.position;
+            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+        }
+
+    }
+
+
+}
