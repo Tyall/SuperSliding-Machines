a655754c3e30a89040d1b83cd1bd4faa0baca372
Dawid Stefański <ddstefanski@gmail.com>
2020-07-21 04:34:19 +0200

    Level description
    -Added panels with level description

--- Assets/Scripts/LevelButton.cs
+++ Assets/Scripts/LevelButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -10,19 +11,28 @@ public class LevelButton : MonoBehaviour
     [HideInInspector] public int levelNum;
     public TextMeshProUGUI buttonText;
 
-    
+    private GameObject levelDetails;
+
+
+
     void Start()
     {
         levelNum = int.Parse(buttonText.text);
+        levelDetails = GameObject.Find("LevelDetails");
     }
 
     public void LoadLevel()
     {
         Debug.Log(levelNum);
-        // SceneManager.LoadScene(levelNum);
-        //GameObject.Find("Level Details").SetActive(true);
-        //GameObject.Find("Level 1 Details").SetActive(true);
-        
+        //SceneManager.LoadScene(levelNum);
+
+        levelDetails.GetComponent<CanvasGroup>().LeanAlpha(1, 0.2f);// = 1;
+        levelDetails.GetComponent<CanvasGroup>().interactable = true;
+        levelDetails.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        //Make a loop for every level button to handle its desired level desc etc.
+
+
 
         //TODO
         //Instead of loading level right after clicking the button, open a pop-up explaining
